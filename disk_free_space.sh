9074e42cad19a2467d8bd86cf1415384a3cc5af5
KZ <kubedzero@gmail.com>
2020-09-18 15:18:35 -0700

    Change var names in share free space, introduce disk free space

---
+++ disk_free_space.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+# This uses df to find the free space of physical disks
+
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
+
+# Run df with only the columns we want, mount point and available kibibytes
+df_output=$(df --output=target --output=avail)
+
+# Filter the output to only the lines we want, those with /boot or /mnt/disk*
+# https://phoenixnap.com/kb/grep-multiple-strings
+filtered_df_lines=$(grep -e '/mnt/disk' -e '/boot' <<< "$df_output")
+
+# Iterate over each line of output
+# https://unix.stackexchange.com/questions/9784/how-can-i-read-line-by-line-from-a-variable-in-bash
+while IFS= read -r line
+do
+
+    # Get the clean name, or everything after the last slash
+    # https://unix.stackexchange.com/questions/247560/print-everything-after-a-slash
+    line=$(echo "$line" | sed 's:.*/::')
+
+    # Use awk to get the desired information
+    disk_name=$(echo "$line" | awk '{print $1}')
+    disk_free_kib=$(echo "$line" | awk '{print $2}')
+
+    # Exit early if Name is empty or free space is not numeric
+    # https://unix.stackexchange.com/questions/151654/checking-if-an-input-number-is-an-integer
+    if [[ -z "$disk_name" || ! $disk_free_kib =~ ^[0-9]+$ ]]
+    then
+        $ECHO "Exiting: Encountered an empty disk name or non-numeric free space"
+        exit 1
+    fi
+
+    # Multiply KiB by 1024 to get bytes
+    disk_free=$((disk_free_kib * 1024))
+
+    # Output the final string to STDOUT
+    echo "$disk_name: $disk_free"
+
+    # Printf '%s\n' "$var" is necessary because printf '%s' "$var" on a
+    # variable that doesn't end with a newline then the while loop will
+    # completely miss the last line of the variable.
+done < <(printf '%s\n' "$filtered_df_lines")
+
+# Exit normally with 0 signal
+exit 0
--- share_free_space.sh
+++ share_free_space.sh
@@ -6,51 +6,52 @@
 set -euo pipefail
 
 # Define the input file from which we will read Share name and free KB
-iniInput=/var/local/emhttp/shares.ini
+ini_input=/var/local/emhttp/shares.ini
 
 # Store Share names and KB free
 # Find names by getting lines with an open bracket "["
-shareNames=$(grep '\[' $iniInput)
+share_names=$(grep '\[' $ini_input)
 # Find kilobytes free by getting lines with string "free="
-shareFree=$(grep 'free=' $iniInput)
+share_free=$(grep 'free=' $ini_input)
 
-# Store the line count of shareNames 
-shareNameCount=$(echo "$shareNames" | wc -l)
+# Store the line count of share_names
+share_name_count=$(echo "$share_names" | wc -l)
 
 # If counts differ, exit early
-if [ "$shareNameCount" -ne "$(echo "$shareNames" | wc -l)" ]
+if [ "$share_name_count" -ne "$(echo "$share_names" | wc -l)" ]
 then
-  echo "Exiting: Name count [$shareNameCount] differs from Free count"
-  exit 1
+    echo "Exiting: Name count [$share_name_count] differs from Free count"
+    exit 1
 fi
 
 # Iterate through each name/size element pair
 # https://github.com/koalaman/shellcheck/wiki/SC2004 no need for $
-for (( i=1; i<=shareNameCount; i++ ));
+for (( i=1; i<=share_name_count; i++ ));
 do
 
-  # Name stored as ["myShareName"] with surrounding double quotes and brackets
-  # https://stackoverflow.com/questions/15777232/how-can-i-echo-print-specific-lines-from-a-bash-variable
-  # Remove the opening [" and the closing "]. In sed, \[" matches open [" 
-  # \(.*\) groups the middle, "\] matches end "], \1 outputs the middle group  
-  share_name=$(sed -n "${i}"p <<< "$shareNames" | sed 's/\["\(.*\)"\]/\1/')
+    # Name stored as ["myShareName"] with surrounding double quotes and brackets
+    # https://stackoverflow.com/questions/15777232/how-can-i-echo-print-specific-lines-from-a-bash-variable
+    # Remove the opening [" and the closing "]. In sed, \[" matches open ["
+    # \(.*\) groups the middle, "\] matches end "], \1 outputs the middle group
+    share_name=$(sed -n "${i}"p <<< "$share_names" | sed 's/\["\(.*\)"\]/\1/')
 
-  # Free space stored as free="12345" with free=" at start and " at end
-  free_kb=$(sed -n "${i}"p <<< "$shareFree" | sed 's/free="\(.*\)"/\1/')
+    # Free space stored as free="12345" with free=" at start and " at end
+    free_kibibytes=$(sed -n "${i}"p <<< "$share_free" | sed 's/free="\(.*\)"/\1/')
 
-  # Exit early if Share Name is empty or free space is not numeric
-  # https://unix.stackexchange.com/questions/151654/checking-if-an-input-number-is-an-integer
-  if [[ -z "$share_name" || ! $free_kb =~ ^[0-9]+$ ]]
-  then
-    $ECHO "Exiting: Encountered an empty share name or non-numeric free space"
-    exit 1
-  fi
+    # Exit early if Share Name is empty or free space is not numeric
+    # https://unix.stackexchange.com/questions/151654/checking-if-an-input-number-is-an-integer
+    if [[ -z "$share_name" || ! $free_kibibytes =~ ^[0-9]+$ ]]
+    then
+        $ECHO "Exiting: Encountered an empty share name or non-numeric free space"
+        exit 1
+    fi
 
-  # Convert to bytes, avoid kilobyte (1000 bytes) kibibyte (1024 bytes) misuse
-  free_bytes=$(($free_kb * 1024))
+    # Convert to bytes, avoid kilobyte (1000 bytes) kibibyte (1024 bytes) misuse
+    # https://github.com/koalaman/shellcheck/wiki/SC2004 no need for $
+    free_bytes=$((free_kibibytes * 1024))
 
-  # Print current share's name and free KB on a line for SNMP to grab
-  echo "$share_name: $free_bytes"
+    # Print current share's name and free KB on a line for SNMP to grab
+    echo "$share_name: $free_bytes"
 
 done
 
