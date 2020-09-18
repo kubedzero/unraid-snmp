c0a597e3dc749c4792897342d92f41662e769aa4
KZ <kubedzero@gmail.com>
2020-09-18 13:09:48 -0700

    Updated free space script

--- share_free_space.sh
+++ share_free_space.sh
@@ -1,27 +1,58 @@
 #!/usr/bin/bash
 
-ECHO=/usr/bin/echo
-GREP=/usr/bin/grep
-SED=/usr/bin/sed
+# This uses the Unraid-provided shares.ini to print out free KB per share
 
-SHARES_INI=/var/local/emhttp/shares.ini
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
 
-$GREP '\(\[\|free=\)' /var/local/emhttp/shares.ini | while read -r share_name
+# Define the input file from which we will read Share name and free KB
+iniInput=/var/local/emhttp/shares.ini
+
+# Store Share names and KB free
+# Find names by getting lines with an open bracket "["
+shareNames=$(grep '\[' $iniInput)
+# Find kilobytes free by getting lines with string "free="
+shareFree=$(grep 'free=' $iniInput)
+
+# Store the line count of shareNames 
+shareNameCount=$(echo "$shareNames" | wc -l)
+
+# If counts differ, exit early
+if [ "$shareNameCount" -ne "$(echo "$shareNames" | wc -l)" ]
+then
+  echo "Exiting: Name count [$shareNameCount] differs from Free count"
+  exit 1
+fi
+
+# Iterate through each name/size element pair
+# https://github.com/koalaman/shellcheck/wiki/SC2004 no need for $
+for (( i=1; i<=shareNameCount; i++ ));
 do
-  read -r free_kb
 
-  share_name=$($ECHO $share_name | $SED 's/\["\(.*\)"\]/\1/')
-  free_kb=$($ECHO $free_kb | $SED 's/free="\(.*\)"/\1/')
+  # Name stored as ["myShareName"] with surrounding double quotes and brackets
+  # https://stackoverflow.com/questions/15777232/how-can-i-echo-print-specific-lines-from-a-bash-variable
+  # Remove the opening [" and the closing "]. In sed, \[" matches open [" 
+  # \(.*\) groups the middle, "\] matches end "], \1 outputs the middle group  
+  share_name=$(sed -n "${i}"p <<< "$shareNames" | sed 's/\["\(.*\)"\]/\1/')
 
-  free_bytes=$(($free_kb * 1024))
+  # Free space stored as free="12345" with free=" at start and " at end
+  free_kb=$(sed -n "${i}"p <<< "$shareFree" | sed 's/free="\(.*\)"/\1/')
 
-  if [[ -z "$share_name" || ! $free_bytes =~ ^[0-9]+$ ]]
+  # Exit early if Share Name is empty or free space is not numeric
+  # https://unix.stackexchange.com/questions/151654/checking-if-an-input-number-is-an-integer
+  if [[ -z "$share_name" || ! $free_kb =~ ^[0-9]+$ ]]
   then
-    $ECHO "ERROR! Couldn't parse $SHARES_INI"
+    $ECHO "Exiting: Encountered an empty share name or non-numeric free space"
     exit 1
   fi
 
-  $ECHO "$share_name: $free_bytes"
+  # Convert to bytes, avoid kilobyte (1000 bytes) kibibyte (1024 bytes) misuse
+  free_bytes=$(($free_kb * 1024))
+
+  # Print current share's name and free KB on a line for SNMP to grab
+  echo "$share_name: $free_bytes"
+
 done
 
+# Exit normally with 0 signal
 exit 0
