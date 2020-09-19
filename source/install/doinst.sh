f8fa5e114a522379d44e70459611307497136cb1
KZ <kubedzero@gmail.com>
2020-09-18 23:43:32 -0700

    Move most of setup to doinst.sh, continuing refinement

--- source/install/doinst.sh
+++ source/install/doinst.sh
@@ -19,3 +19,34 @@ chmod a+x share_free_space.sh
 chmod a+r snmpd.conf
 chmod a+r snmp.png
 chmod a+r README.md
+
+# Stop SNMP daemon if it is currently running
+bash /etc/rc.d/rc.snmpd stop
+
+# Replace the default SNMP config with our own, backing up the original
+mv --backup /usr/local/emhttp/plugins/snmp/snmpd.conf /etc/snmp/snmpd.conf
+
+# Define the additional flags we want to add into the SNMP daemon startup
+# Spaces at beginning and end of string to separate from other flags
+# 1=a=alert, 2=c=crit, 3=e=err, 4=w=warn, 5=n=notice, 6=i=info, 7=d=debug
+new_flags=" -L6f /var/log/snmp/snmp.log "
+
+# Get existing OPTIONS from file, keeping only what's in double quotes
+# https://stackoverflow.com/questions/35636323/extracting-a-string-between-two-quotes-in-bash
+options=$(grep "OPTIONS=" /etc/rc.d/rc.snmpd | cut -d'"' -f 2)
+# Check that flags haven't already been added
+if [[ $options != *"-L"* ]]; then
+    # Concatenate the new flags with the old
+    options=$new_flags$options
+    # Replace the line beginning with OPTIONS= with a custom set
+    # Use a custom delimiter | to avoid collisions of sed and variable use of /
+    # Escape the start quote and end quote when we recreate the line
+    # https://stackoverflow.com/questions/9366816/sed-fails-with-unknown-option-to-s-error
+    sed --in-place=.bak --expression "s|^OPTIONS=.*|OPTIONS=\"$options\"|" /etc/rc.d/rc.snmpd
+fi
+
+# Restart SNMP daemon now that we've adjusted how it starts up
+bash /etc/rc.d/rc.snmpd start
+
+# Exit with the code of the SNMP daemon startup
+exit $?
