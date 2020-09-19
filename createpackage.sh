b8f47932fd5a77d4759ab32f5cd31dbe5b00de92
KZ <kubedzero@gmail.com>
2020-09-19 00:44:50 -0700

    Update GPL v2 to v3, minor updates to createpackage.sh

--- createpackage.sh
+++ createpackage.sh
@@ -11,7 +11,12 @@ package_name="unraid-snmp"
 echo "Version defined as [$1]"
 echo "Source code directory defined as [$2]"
 
-file_name=$(printf "%s-%s-x86_64-1.txz" "$package_name" "$1")
-
-echo "Creating Slackware package $(pwd)/$file_name"
-makepkg --linkadd y --chown n "$file_name"
+# Check that makepkg is available
+# https://stackoverflow.com/questions/26675681/how-to-check-the-exit-status-using-an-if-statement
+if command -v makepkg; then
+    file_name=$(printf "%s-%s-x86_64-1.txz" "$package_name" "$1")
+    echo "Creating Slackware package $(pwd)/$file_name"
+    makepkg --linkadd y --chown n "$file_name"
+else
+    echo "Binary makepkg not found, is your OS Slackware?"
+fi
