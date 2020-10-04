9e13942f7b351fb0772f34387330263ea944dfc2
KZ <kubedzero@gmail.com>
2020-10-04 09:31:14 -0700

    Update disk temp for clearer file naming

--- source/usr/local/emhttp/plugins/snmp/disk_temps.sh
+++ source/usr/local/emhttp/plugins/snmp/disk_temps.sh
@@ -7,13 +7,15 @@
 set -euo pipefail
 
 # Define the working directory in which files will live
-working_dir=/tmp/plugins/snmp/
+working_dir="/tmp/plugins/snmp/"
 # Define the file that will hold disk temperatures
-cache_file_name=disk_temps.txt
+cache_file_name="disk_temps.txt"
 # Define the lock file that will prevent multiple simultaneous writes
-cache_lock_name=disk_temps.lock
+cache_lock_name="disk_temps.lock"
 # Define the log output for errors
-cache_log_name=disk_temps.log
+cache_log_name="disk_temps.log"
+# Define the file that the SNMP Settings page updates
+config_file="/boot/config/plugins/snmp/snmp.cfg"
 
 # Update file name variables with full paths
 cache_file_full_path="$working_dir$cache_file_name"
@@ -67,11 +69,11 @@ function update_cache_file {
     # source will load the lines of the .cfg as variables
     # Exit flag is 1 when file missing, so provide an OR true to offer an exit 0
     # 2> /dev/null hides STDERR and sends it to /dev/null instead
-    # Set the variables we expect to get beforehand, so we know a base value exists
-    source "/boot/config/plugins/snmp/snmp.cfg" 2> /dev/null || true
+    source "$config_file" 2> /dev/null || true
 
     # Check if the variable sourced from the .cfg is equal to 1/enabled
     # Provide default value if variable is unset, to avoid breaking `set`
+    # https://stackoverflow.com/questions/2013547/assigning-default-values-to-shell-variables-with-a-single-command-in-bash
     if [[ "${UNSAFETEMP:-}" -eq "1" ]]
     then
         echo "Disk temp retrieval may wake disks from STANDBY"
