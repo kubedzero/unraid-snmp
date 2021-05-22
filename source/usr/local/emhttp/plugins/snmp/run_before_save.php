a9907710d4ffb303d26bc0cffb82df777dc2ac61
KZ <kubedzero@gmail.com>
2021-05-21 22:27:38 -0700

    More polish on .page file

--- source/usr/local/emhttp/plugins/snmp/run_before_save.php
+++ source/usr/local/emhttp/plugins/snmp/run_before_save.php
@@ -1,7 +1,8 @@
 <?php
 if (!empty($_REQUEST['SNMPDCONF'])) {
-    // SNMPD is restarted through the HTML form by also supplying "#commands"
+    // Copy the text box content into the live config file used by SNMP
     file_put_contents('/etc/snmp/snmpd.conf', $_REQUEST['SNMPDCONF']);
-    // Also write to USB so we can restore this file after update / reboot
+    // Also write to USB so package installation can use it after reboot/update
     file_put_contents('/boot/config/plugins/snmp/snmpd.conf', $_REQUEST['SNMPDCONF']);
 }
+?>
